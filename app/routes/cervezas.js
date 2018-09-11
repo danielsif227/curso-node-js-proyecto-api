1ff0ab7abb8fed49be807417af5af58e79f837a9
danielsif227 <danielsif227@gmail.com>
2018-09-11 12:21:58 +0200

    Enrutador funcionando

---
+++ app/routes/cervezas.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+  res.json({ mensaje: 'Listado de cervezas' })
+})
+
+router.post('/', (req, res) => {
+  res.json({ mensaje: 'Cerveza guardada' })
+})
+
+router.delete('/', (req, res) => {
+  res.json({ mensaje: 'Cerveza borrada' })
+})
+
+module.exports = router
--- app/server.js
+++ app/server.js
@@ -1,24 +1,19 @@
-var express = require('express') // llamamos a Express
-var app = express()
+const express = require('express') // llamamos a Express
+const app = express()
 
-var port = process.env.PORT || 8080 // establecemos nuestro puerto
+const router = require('./routes') // coje el index.js de dentro de la carpeta
+
+const port = process.env.PORT || 8080 // establecemos nuestro puerto
 
 app.get('/', (req, res) => {
   res.json({ mensaje: '¡Hola Mundo!' })
 })
 
-app.get('/cervezas', (req, res) => {
-  res.json({ mensaje: '¡A beber cerveza!' })
-})
-
-app.post('/cervezas', (req, res) => {
-  res.send('Cerveza guardada');
-});
-
-app.delete('/cervezas', (req, res) => {
-  res.send('Cerveza borrada');
-});
+// nuestra ruta irá en http://localhost:8080/api
+// es bueno que haya un prefijo, sobre todo por el tema de versiones de la API
+app.use('/api', router)
 
 // iniciamos nuestro servidor
-app.listen(port)
-console.log('API escuchando en el puerto ' + port)
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
